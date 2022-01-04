cfa96e2d8c5f501e9b1dd0f94236d05f5659492f
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-01-04 09:28:17 -0300

    fix: env change

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: process.env.GATSBY_STRAPI_URL,
+        apiURL: `https://strapi.redait.cloudspaceclient.com`,
         queryLimit: 1000, // Defaults to 100
         collectionTypes: [`companies`],
         // singleTypes: [`home-page`, `contact`],
