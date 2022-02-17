0a3702fd6f949b2b51e70dc357d0bb7e2ead65ab
Diego Perez <diego.perez@nybblegroup.com>
2022-02-17 18:23:18 -0300

    avances en profile page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,12 +2,13 @@ import * as React from 'react';
 
 import Layout from '../components/ui/layout';
 import Seo from '../components/seo';
-
+import Hero from '../components/home/Hero';
 import ProfilePage from "../components/profile/ProfilePage";
 
 const Profile = () => (
   <Layout>
     <Seo title="Perfil" />
+    <Hero />
     <ProfilePage />
   </Layout>
 )
